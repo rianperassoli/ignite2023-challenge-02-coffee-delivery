85997207c527efac8ec291ac3cb086c79a489bf2
Rian Perassoli <rian.perassoli@gmail.com>
2023-10-31 22:51:09 -0300

    Start creating pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
+
 function App() {
-  return <h1>Coffee delivery</h1>
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ThemeProvider>
+  )
 }
 
 export default App
